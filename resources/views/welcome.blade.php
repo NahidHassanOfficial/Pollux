5a50b9fa4b9e68415d959c4c27d29789191588d9
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2025-02-05 19:37:41 +0600

    favicon updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="logo.svg" type="image/x-icon">
     <title>Pollux - Simple Polling System</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
