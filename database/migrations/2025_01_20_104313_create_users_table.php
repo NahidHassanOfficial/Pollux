27958b18cc308cafd0ab2f197e44777814bc6fa4
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2025-02-07 18:47:11 +0600

    users migration updated: added unique and index to usrname column and removed email nullable

--- database/migrations/2025_01_20_104313_create_users_table.php
+++ database/migrations/2025_01_20_104313_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->nullable()->unique();
+            $table->string('username')->unique()->index();
+            $table->string('email')->unique();
             $table->string('otp')->default(0);
             $table->string('password')->nullable();
             $table->string('google_id')->nullable();
