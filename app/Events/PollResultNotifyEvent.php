47982e0173d2e77fd495cd4b27b44ad73579b7e1
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2025-02-10 20:42:50 +0600

    removed old implementation

--- app/Events/PollResultNotifyEvent.php
+++ app/Events/PollResultNotifyEvent.php
@@ -1,14 +1,11 @@
 <?php
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PollResultNotifyEvent implements ShouldBroadcastNow
+class PollResultNotifyEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -21,29 +18,4 @@ class PollResultNotifyEvent implements ShouldBroadcastNow
         $this->poll = $poll;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('pollEnd-Notification.' . $this->poll->user_id),
-        ];
-    }
-
-    public function broadcastWith()
-    {
-        return [
-            'poll_uid' => $this->poll->poll_uid,
-            'title'    => $this->poll->title,
-            'message'  => 'This poll has been ended. Check your mail for result or visit the poll',
-        ];
-    }
-
-    public function broadcastAs()
-    {
-        return 'pollEndEvent';
-    }
 }
